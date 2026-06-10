dd0b175a38b3d3d2f51070d768b0874daf898f32
Pablo Perez <pabloperezz160rgmail.com>
2026-06-11 00:01:43 +0200

    Implementado test para el constructor vacio de la clase Circulo

---
+++ src/test/java/com/dam/practicajunit/CirculoTest.java
@@ -0,0 +1,13 @@
+package com.dam.practicajunit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CirculoTest {
+
+    @Test 
+    public void testConstructorVacio() {
+        Circulo c = new Circulo();
+        assertEquals(0.0, c.getRadio(), 0.0);
+    }
+}
